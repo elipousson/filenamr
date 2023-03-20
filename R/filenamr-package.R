c90364196bf53e1e0f3d20160403dee7a647664e
Eli Pousson <eli.pousson@gmail.com>
2023-03-20 10:55:15 -0400

    refactor: update imports + NAMESPACE

--- R/filenamr-package.R
+++ R/filenamr-package.R
@@ -3,6 +3,17 @@
 
 ## usethis namespace: start
 #' @importFrom rlang %||%
+#' @importFrom rlang is_character
+#' @importFrom rlang is_null
+#' @importFrom rlang caller_arg
+#' @importFrom rlang caller_env
+#' @importFrom rlang env_get_list
+#' @importFrom rlang is_missing
+#' @importFrom rlang is_vector
+#' @importFrom rlang is_list
+#' @importFrom rlang is_string
+#' @importFrom rlang is_na
+#' @importFrom rlang abort
 #' @importFrom cli cli_abort cli_warn cli_inform
 #' @importFrom cliExtras cli_abort_ifnot cli_warn_ifnot cli_inform_ifnot
 #'   cli_yesno
