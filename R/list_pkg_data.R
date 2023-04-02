fea44a908751bea5e89b8c465c99cbfe2fa39bb0
Eli Pousson <eli.pousson@gmail.com>
2023-04-01 22:45:38 -0400

    refactor: modify list_pkg_datasets to use data.rds
    - refactor: modify list_pkg_datasets to use data.rds
    - refactor: add list_system_files helper function

--- R/list_pkg_data.R
+++ R/list_pkg_data.R
@@ -11,55 +11,87 @@
 #' @importFrom rlang caller_env check_installed
 list_pkg_data <- function(pkg = NULL,
                           version = NULL,
+                          dir = "extdata",
+                          lib.loc = NULL,
                           call = caller_env()) {
   rlang::check_installed(pkg, version = version, call = call)
 
   rbind(
-    list_pkg_datasets(pkg),
-    list_pkg_extdata(pkg),
+    list_pkg_datasets(pkg, lib.loc = lib.loc),
+    list_pkg_extdata(pkg, dir = dir),
     list_pkg_cachedata(pkg)
   )
 }
 
 #' @name list_pkg_datasets
 #' @rdname list_pkg_data
+#' @inheritParams base::system.file
 #' @export
 #' @importFrom utils data
-list_pkg_datasets <- function(pkg, envir = .GlobalEnv) {
+#' @importFrom rlang set_names
+list_pkg_datasets <- function(pkg,
+                              lib.loc = NULL) {
+  check_string(pkg)
   data_files <-
     as.data.frame(
-      utils::data(package = pkg, envir = envir)[["results"]]
+      readRDS(system.file("Meta", "data.rds", package = pkg, lib.loc = lib.loc))
     )
-  data_files <- data_files[, c("Package", "Item", "LibPath")]
-  names(data_files) <- c("package", "item", "path")
-  data_files
+
+  data_files <-
+    cbind(
+      data_files[, c(2, 1)],
+      rep_len(pkg, nrow(data_files))
+    )
+
+  rlang::set_names(data_files, c("item", "path", "package"))
 }
 
 #' @name list_pkg_extdata
 #' @rdname list_pkg_data
+#' @param dir One or more directory names to pass as the first argument of
+#'   [system.file()]. Defaults to "extdir".
+#' @inheritParams base::list.files
 #' @export
 #' @importFrom rlang is_empty
 list_pkg_extdata <- function(pkg,
+                             dir = "extdata",
                              full.names = TRUE,
                              recursive = TRUE) {
-  extdata_dir <- system.file("extdata", package = pkg)
+  check_string(pkg)
+  if (length(dir) > 1) {
+    list_system_files <- Vectorize(list_system_files, "dir", TRUE, FALSE)
+  }
 
-  extdata_files <- list.files(
-    path = extdata_dir,
-    recursive = recursive,
-    full.names = full.names
-  )
+  extdata_files <- list_system_files(pkg, dir, recursive, full.names)
 
   if (!rlang::is_empty(extdata_files)) {
     extdata_files <-
       cbind(
-        data.frame("package" = rep_len("mapbaltimore", length(extdata_files))),
+        data.frame("item" = str_remove_fileext(basename(extdata_files))),
         data.frame("path" = extdata_files),
-        data.frame("item" = str_remove_fileext(basename(extdata_files)))
+        data.frame("package" = rep_len(pkg, length(extdata_files)))
       )
   }
 
   extdata_files
+}
+
+#' List system files for a package
+#'
+#' @noRd
+list_system_files <- function(pkg,
+                              dir = "extdata",
+                              full.names = TRUE,
+                              recursive = TRUE,
+                              include.dirs = FALSE,
+                              ...) {
+  list.files(
+    path = system.file(dir, package = pkg),
+    recursive = recursive,
+    full.names = full.names,
+    include.dirs = include.dirs,
+    ...
+  )
 }
 
 #' @name list_pkg_cachedata
@@ -70,6 +102,7 @@ list_pkg_extdata <- function(pkg,
 list_pkg_cachedata <- function(pkg,
                                full.names = TRUE,
                                recursive = TRUE) {
+  check_string(pkg)
   cache_dir <- rappdirs::user_cache_dir(pkg)
 
   cache_files <- list.files(
@@ -81,9 +114,9 @@ list_pkg_cachedata <- function(pkg,
   if (!rlang::is_empty(cache_files)) {
     cache_files <-
       cbind(
-        data.frame("package" = rep_len("mapbaltimore", length(cache_files))),
+        data.frame("item" = str_remove_fileext(basename(cache_files))),
         data.frame("path" = cache_files),
-        data.frame("item" = str_remove_fileext(basename(cache_files)))
+        data.frame("package" = rep_len(pkg, length(cache_files)))
       )
   }
 
