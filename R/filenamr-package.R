94f7157d37ecf2b64c01ac4b7a6e65afd616ca5c
Eli Pousson <eli.pousson@gmail.com>
2023-05-12 21:20:43 -0400

    refactor: replace purr w/ standalone-purrr
    - refactor: replace purr w/ standalone-purrr + add lubridate + tibble to Suggests

--- R/filenamr-package.R
+++ R/filenamr-package.R
@@ -2,27 +2,8 @@
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang %||%
-#' @importFrom rlang is_character
-#' @importFrom rlang is_null
-#' @importFrom rlang caller_arg
-#' @importFrom rlang caller_env
-#' @importFrom rlang env_get_list
-#' @importFrom rlang is_missing
-#' @importFrom rlang is_vector
-#' @importFrom rlang is_list
-#' @importFrom rlang is_false
-#' @importFrom rlang is_string
-#' @importFrom rlang is_empty
-#' @importFrom rlang is_na
-#' @importFrom rlang na_chr
-#' @importFrom rlang has_length
-#' @importFrom rlang has_name
-#' @importFrom rlang set_names
-#' @importFrom rlang is_interactive
-#' @importFrom rlang check_installed
-#' @importFrom rlang check_required
-#' @importFrom rlang abort
+#' @import rlang
+#' @importFrom rlang %||% as_function global_env set_names inject
 #' @importFrom cli cli_abort cli_warn cli_inform
 #' @importFrom cliExtras cli_abort_ifnot cli_warn_ifnot cli_inform_ifnot
 #'   cli_yesno
